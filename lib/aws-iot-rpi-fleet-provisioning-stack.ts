56166c7a391056a6b2e956d53e3ebbbe0f6574b2
Arman <zhakypbekov.arman@gmail.com>
2021-03-27 19:58:24 -0700

    save

--- lib/aws-iot-rpi-fleet-provisioning-stack.ts
+++ lib/aws-iot-rpi-fleet-provisioning-stack.ts
@@ -226,6 +226,7 @@ export class AwsIotRpiFleetProvisioningStack extends cdk.Stack {
               'mount /dev/mapper/${BOOT_DISK} $SDCARD_MOUNT',
               // Configure Wifi
               `echo "
+# TIMEZONE = \\"$TIMEZONE\\"
 ctrl_interface=DIR=/var/run/wpa_suplicant GROUP=netdev
 update_config=1
 country=$WIFI_COUNTRY
@@ -241,77 +242,6 @@ network={
               'umount "$SDCARD_MOUNT"',
               // Mount root disk
               'mount /dev/mapper/${ROOT_DISK} $SDCARD_MOUNT',
-              //Set raspi-config
-              `echo "
-# TIMEZONE = \\"$TIMEZONE\\"
-# For more options and information see
-# http://rpf.io/configtxt
-# Some settings may impact device functionality. See link above for details
-
-# uncomment if you get no picture on HDMI for a default "safe" mode
-#hdmi_safe=1
-
-# uncomment this if your display has a black border of unused pixels visible
-# and your display can output without overscan
-#disable_overscan=1
-
-# uncomment the following to adjust overscan. Use positive numbers if console
-# goes off screen, and negative if there is too much border
-#overscan_left=16
-#overscan_right=16
-#overscan_top=16
-#overscan_bottom=16
-
-# uncomment to force a console size. By default it will be display's size minus
-# overscan.
-#framebuffer_width=1280
-#framebuffer_height=720
-
-# uncomment if hdmi display is not detected and composite is being output
-#hdmi_force_hotplug=1
-
-# uncomment to force a specific HDMI mode (this will force VGA)
-#hdmi_group=1
-#hdmi_mode=1
-
-# uncomment to force a HDMI mode rather than DVI. This can make audio work in
-# DMT (computer monitor) modes
-#hdmi_drive=2
-
-# uncomment to increase signal to HDMI, if you have interference, blanking, or
-# no display
-#config_hdmi_boost=4
-
-# uncomment for composite PAL
-#sdtv_mode=2
-
-#uncomment to overclock the arm. 700 MHz is the default.
-#arm_freq=800
-
-# Uncomment some or all of these to enable the optional hardware interfaces
-#dtparam=i2c_arm=on
-#dtparam=i2s=on
-#dtparam=spi=on
-
-# Uncomment this to enable infrared communication.
-#dtoverlay=gpio-ir,gpio_pin=17
-#dtoverlay=gpio-ir-tx,gpio_pin=18
-
-# Additional overlays and parameters are documented /boot/overlays/README
-
-# Enable audio (loads snd_bcm2835)
-dtparam=audio=on
-
-[pi4]
-# Enable DRM VC4 V3D driver on top of the dispmanx display stack
-dtoverlay=vc4-fkms-v3d
-max_framebuffers=2
-
-[all]
-#dtoverlay=vc4-fkms-v3d
-start_x=1
-gpu_mem=128
-" > "$SDCARD_MOUNT/boot/config.txt"`,
               // Change the sshd_config file to disable password authentication
               `sed -e 's;^#PasswordAuthentication.*$;PasswordAuthentication no;g' -e 's;^PermitRootLogin .*$;PermitRootLogin no;g' -i "$SDCARD_MOUNT/etc/ssh/sshd_config"`,
               // Add the ssh public key to the list of authorized keys
