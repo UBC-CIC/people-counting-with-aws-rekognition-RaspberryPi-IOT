787dfc234e5a35abc2be4a72b28c76303ef20001
Arman <zhakypbekov.arman@gmail.com>
2021-04-22 15:06:21 -0700

    save

--- RaspberrypiNodeJSApplication/privateSetup.sh
+++ RaspberrypiNodeJSApplication/privateSetup.sh
@@ -1,6 +1,9 @@
+cd /home/pi/Desktop/
+sudo rm -rf rpi
+git clone https://github.com/UBC-CIC/people-counting-with-aws-rekognition-RaspberryPi-IOT.git rpi
 cd /home/pi/Desktop/iot
-sudo cp ./* /home/pi/Desktop/people-counting-with-aws-rekognition-RaspberryPi-IOT/RaspberrypiNodeJSApplication
-cd /home/pi/Desktop/people-counting-with-aws-rekognition-RaspberryPi-IOT/RaspberrypiNodeJSApplication
+sudo cp ./* /home/pi/Desktop/rpi/RaspberrypiNodeJSApplication
+cd /home/pi/Desktop/rpi/RaspberrypiNodeJSApplication
 mkdir certs
 npm install
 sudo mv ./certificate.pem ./certs/certificate.pem
